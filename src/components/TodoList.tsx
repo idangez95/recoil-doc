272e61d08c7fca21c870021e849f0729bbebae0f
idangez95 <idangez95@gmail.com>
2021-08-16 15:43:01 +0300

    first commit

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,24 @@
+import { atom, useRecoilValue } from "recoil";
+import { TodoItem } from "./TodoItem";
+import { TodoItemCreator } from "./TodoItemCreator";
+
+export const todoListState = atom({
+    key: "todoListState",
+    default: [],
+  });
+
+export function TodoList() {
+  const todoList = useRecoilValue(todoListState);
+
+  return (
+    <>
+      {/* <TodoListStats /> */}
+      {/* <TodoListFilters /> */}
+      <TodoItemCreator />
+
+      {todoList.map((todoItem) => (
+        <TodoItem key={todoItem?.id} item={todoItem} />
+      ))}
+    </>
+  );
+}
---
+++ src/type.tsx
@@ -0,0 +1,5 @@
+
+export interface ITodoList {
+    key: string;
+    default: [];
+}
